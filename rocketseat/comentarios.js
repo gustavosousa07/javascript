ca8b17a854c69fe750f9dcc2ad2fe93eda58f1c6
gustavosousa07 <138171453+gustavosousa07@users.noreply.github.com>
2023-11-08 15:01:03 -0300

    aula 1 concluida

---
+++ rocketseat/comentarios.js
@@ -0,0 +1,12 @@
+//aqui tenho um comentario e linha 
+
+/*  Aqui eu tenho um comentario multi linha
+    o codigo abaixo escreve alguma mensagem no devtools
+*/
+
+
+
+
+
+console.log("Bem vindos ao Starter") //alguma coisa
+console.log('não iria ignorar')
